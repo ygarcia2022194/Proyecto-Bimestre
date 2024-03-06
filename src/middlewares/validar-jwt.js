4a58e061d8999c82f68f6c463f8f1191f4437170
ygarcia-2022194 <ygarcia-2022194@kinal.edu.gt>
2024-03-06 14:51:19 -0600

    Se hizo la validacion

---
+++ src/middlewares/validar-jwt.js
@@ -0,0 +1,36 @@
+import { Jwt } from "jsonwebtoken";
+import Admin from '../admin/admin.js';
+import Client from '../client/client.js';
+import {request, response} from 'express';
+
+export const validarJWT = async(req = request, res= response, next)=>{
+    const token = req.header('x-token');
+    if(!token){
+        return res.status(401).json({
+            msg: 'No hay token en la peticion'
+        });
+    }
+
+    try {
+        const {uid} = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        const admin = await Admin.findById(uid);
+        if(!admin){
+            return res.status(401).json({
+                msg: "Not admin"
+            });
+        }
+        if(!admin.estado){
+            return res.status(401).json({
+                msg: "Invalid token, user with false status"
+            });
+        }
+        req.admin = admin;
+        next();
+    } catch (e){
+        console.log(e);
+        res.status(401).json({
+            msg: 'Token no valido'
+        })
+        
+    }
+}
